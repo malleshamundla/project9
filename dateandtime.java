2127324ebc5a3d3ae1aa0658d6e476755d4bfc7e
mallesh26 <malleshamundla26@gmail.com>
2024-12-09 14:34:24 +0530

    dateandtime

---
+++ dateandtime.java
@@ -0,0 +1,10 @@
+import java.time.LocalDateTime;
+
+public class DateTimeExample {
+    public static void main(String[] args) {
+        // Get and display the current date and time
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        System.out.println("Current Date and Time: " + currentDateTime);
+    }
+}
+
